352e27cb7573bd91eeb47f7fc1a920de82884f18
Kinetix <kinetix@isurf.ca>
2024-11-07 12:04:58 -0800

    Attempting to get the plugin to not crash.

--- library.js
+++ library.js
@@ -8,15 +8,14 @@ const meta = require.main.require('./src/meta');
 const routeHelpers = require.main.require('./src/routes/helpers');
 
 const user = require.main.require('./src/user');
-
 const categories = require.main.require('./src/categories');
 
 const plugin = {};
 
-plugin.onUserCreate = async function ({ user }) {
+plugin.onUserCreate = async function ({ hookData }) {
 
   const announcementCategory = 1;
-  await user.setCategoryWatchState(user.uid, [announcementCategory], categories.watchStates.watching);
+  await user.setCategoryWatchState(hookData.user.uid, [announcementCategory], categories.watchStates.watching);
 
 };
 
