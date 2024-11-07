638990dadd79e2453d6dcb3412c96367933a0e0d
Kinetix <kinetix@isurf.ca>
2024-11-06 16:54:08 -0800

    First revision of base working package
    Currently sets new users to watch Category 1, which
    defaults to Announcements on a fresh install.

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@
  *
  * {
  *     "test_plugins": [
- *         "nodebb-plugin-quickstart"
+ *         "nodebb-plugin-auto-set-watch-categories"
  *     ]
  * }
  */
@@ -22,7 +22,7 @@ const assert = require('assert');
 
 const db = require.main.require('./test/mocks/databasemock');
 
-describe('nodebb-plugin-quickstart', () => {
+describe('nodebb-plugin-auto-set-watch-categories', () => {
 	before(() => {
 		// Prepare for tests here
 	});
